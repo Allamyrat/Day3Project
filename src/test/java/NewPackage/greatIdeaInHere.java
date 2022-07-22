ce279cb9c8792a997952d0f1382276f7b165da54
ZohreD <84271729+zohreD@users.noreply.github.com>
2022-07-22 14:22:29 +0700

    i have made some chnages in here let's see the result

--- src/test/java/NewPackage/greatIdeaInHere.java
+++ src/test/java/NewPackage/greatIdeaInHere.java
@@ -3,5 +3,7 @@ package NewPackage;
 public class greatIdeaInHere {
     public static void main(String[] args) {
         System.out.println("great idea is being created");
+        System.out.println("i have made some changes in here " +
+                "let's see the result then");
     }
 }
